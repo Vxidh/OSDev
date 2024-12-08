432ee985fe0359a211a6289766dd96707e0c0150
Vxidh <vaidheeshbharad@gmail.com>
2024-12-08 20:17:56 +0530

    initial commit

---
+++ objdump/hello.c
@@ -0,0 +1,9 @@
+//This is a sample program to test objdump.
+#include<stdio.h>
+
+int main(){
+    printf("Hello World");
+    return 0;
+} 
+
+//Do objdump -d hello.exe in the terminal to get access to all the executable sections of the code. Output is in assembly.
